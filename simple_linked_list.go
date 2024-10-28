0777acbe6bf93c73a0f30dd173f8b6122fd58f8f
Jim Cain <jcain@spelt.io>
2024-10-27 23:14:27 -0400

    Completed

--- simple_linked_list.go
+++ simple_linked_list.go
@@ -1,27 +1,73 @@
 package linkedlist
 
-// Define the List and Element types here.
+import "fmt"
 
+type List struct {
+	head *Element
+}
+
+type Element struct {
+	songId int
+	next   *Element
+}
+
+// New creates and returns a List containing the values in `elements`
 func New(elements []int) *List {
-	panic("Please implement the New function")
+	list := List{}
+
+	for _, songId := range elements {
+		list.Push(songId)
+	}
+
+	return &list
 }
 
-func (l *List) Size() int {
-	panic("Please implement the Size function")
+// Size returns the number of songIds in the List
+func (list *List) Size() int {
+	count := 0
+	for ptr := list.head; ptr != nil; ptr = ptr.next {
+		count += 1
+	}
+	return count
 }
 
-func (l *List) Push(element int) {
-	panic("Please implement the Push function")
+// Push inserts an element at the head of List
+func (list *List) Push(element int) {
+	elem := &Element{songId: element, next: list.head}
+	list.head = elem
 }
 
-func (l *List) Pop() (int, error) {
-	panic("Please implement the Pop function")
+// Pop removes the head element from the List and returns its songId
+func (list *List) Pop() (int, error) {
+	head := list.head
+	if head == nil {
+		return 0, fmt.Errorf("list is empty")
+	}
+	value := head.songId
+	if head.next == nil {
+		list.head = nil
+	} else {
+		list.head = head.next
+	}
+	return value, nil
 }
 
-func (l *List) Array() []int {
-	panic("Please implement the Array function")
+// Array returns an array of songIds from the List
+func (list *List) Array() []int {
+	size := list.Size()
+	result := make([]int, size)
+	for ptr := list.head; ptr != nil; ptr = ptr.next {
+		size -= 1
+		result[size] = ptr.songId
+	}
+	return result
 }
 
-func (l *List) Reverse() *List {
-	panic("Please implement the Reverse function")
+// Reverse returns a List with the songIds in reverse order
+func (list *List) Reverse() *List {
+	newList := List{}
+	for ptr := list.head; ptr != nil; ptr = ptr.next {
+		newList.Push(ptr.songId)
+	}
+	return &newList
 }
